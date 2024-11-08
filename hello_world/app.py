bddf94e674d698f10850283c09d1f8295eee811b
michellehoog <michellehoog@gmail.com>
2024-11-08 15:15:05 -0800

    changed return value

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world. my first canary deployment is here!",
             # "location": ip.text.replace("\n", "")
         }),
     }
